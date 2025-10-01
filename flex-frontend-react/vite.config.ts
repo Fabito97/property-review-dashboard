75ad3a93ec0c9144ccf1b2b275580d517edcf54b
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-10-01 11:08:55 +0100

    modified vite config

--- flex-frontend-react/vite.config.ts
+++ flex-frontend-react/vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  resolve: {
+   resolve: {
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
   build: {
-    outDir: "dist", 
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
   },
   server: {
     fs: {
-      allow: [".."], 
+      // Optional: allow access to files outside root if needed
+      allow: [".."],
     },
   },
+
 });
