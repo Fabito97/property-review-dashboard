8b17c3a2a52ba343d3676919e6c7d4f1702f8991
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2025-10-01 10:54:39 +0100

    modify app configs for deployment

--- flex-frontend-react/app/routes.ts
+++ flex-frontend-react/app/routes.ts
@@ -5,10 +5,4 @@ export default [
   route("/properties", "routes/properties.tsx"),
   route("/properties/:id", "routes/propertyDetail.tsx"),
   route("/reviews", "routes/reviews.tsx"),
-//   route("/property/:id/reviews", "routes/propertyReview.tsx"),    
-//     route("/dashboard", "routes/layouts/dashboardLayout.tsx", [
-//       route("/dashboard/admin", "routes/admin.tsx"), // /dashboard
-//       route("/property/:id/reviews", "routes/propertyReview.tsx"),    
-//   ]),
-
 ] satisfies RouteConfig;
